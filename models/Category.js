978c56f74686ddaf5b24955a2d2c55c4a4d58c10
blur <damienhebert710@gmail.com>
2022-05-29 00:43:01 -0500

    setting up sequelize to link connection.js and server.js

--- models/Category.js
+++ models/Category.js
@@ -10,11 +10,14 @@ Category.init(
           type: DataTypes.INTEGER, 
           allowNull: false, 
           primaryKey: true,
-          autoIncrement: true,
+          autoIncrement: true
       }, 
       category_name: {
-          
+          type: DataTypes.STRING,
+          allowNull: false   
       }
+
+
 
     // define columns
   },
