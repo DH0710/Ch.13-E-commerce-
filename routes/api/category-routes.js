ed756473c0a6c41b0cab04e140a7ae47f5d5ae8a
blur <damienhebert710@gmail.com>
2022-05-29 21:36:51 -0500

    trying to figure out routes for creating a new category, think i have it mostly right

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,21 +4,31 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 // localhost:3001/api/category
 router.get('/', (req, res) => {
-    Category.findAll().then((categoryData) => {
-        res.json(categoryData);
-    });
+  Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+  });
 });
 
 router.get('/:id', (req, res) => {
-Category.findOne().then((categoryData) => {
-  res.json(categoryData);
-});
+  Category.findOne().then((categoryData) => {
+    res.json(categoryData);
+  });
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  // post corresponds to what CRUD method?
+
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then((categoryData) => {
+      res.json(categoryData);
+    });
+
+
 });
 
 router.put('/:id', (req, res) => {
