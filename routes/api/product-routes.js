bfe8cb7a191c7d8b2a8cefe720efc4d6d3caf5d2
blur <damienhebert710@gmail.com>
2022-05-29 23:46:03 -0500

    finished building routes for products file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,18 +5,63 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  }).then((productData) => {
+    res.json(productData);
+  });
 });
+// find all products
+// be sure to include its associated Category and Tag data
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  }).then((productData) => {
+    res.json(productData);
+  });
+
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
 router.post('/', (req, res) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id
+  })
+    .then((productData) => {
+      res.json(productData);
+    });
+
+
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -90,6 +135,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((productData) => {
+      if (!productData);
+    });
+
   // delete one product by its `id` value
 });
 
